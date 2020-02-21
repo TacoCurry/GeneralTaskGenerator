d8b65b0c1ddd23b09cc01ce1995f005a69a9c22d
Jo-Yewon <yeawonjo@gmail.com>
2020-02-22 00:30:15 +0900

    refactor: 코드 포맷팅 및 완료 후 결과 출력하도록 수정

--- input.py
+++ input.py
@@ -1,5 +1,9 @@
+def error(msg):
+    print(msg)
+    exit(0)
+
+
 def get_input(input_file="input.txt"):
-    # TODO input.txt에서 읽어온 4개의 값 리턴하도록 해주세요
     try:
         with open(input_file, "r", encoding='UTF8') as f:
             sim_time = int(f.readline())
@@ -9,7 +13,6 @@ def get_input(input_file="input.txt"):
 
             bt_min = int(temp[0])
             bt_max = int(temp[1])
-
 
             print("=======================================================")
             print("This is the Task Generation Input")
@@ -27,8 +30,6 @@ def get_input(input_file="input.txt"):
             print(bt_max)
 
             return sim_time, arr_rate, bt_min, bt_max
-
-
 
     except FileNotFoundError:
         error("task 정보 파일을 찾을 수 없습니다.")
--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import random
 sim_time, arr_rate, bt_min, bt_max = get_input()
 
 with open('generated.txt', 'w', encoding='utf-8') as f:
+    count = 0
     for cur_time in range(sim_time):
         if random.random() <= arr_rate:
             f.write("{} {}\n".format(cur_time, random.randint(bt_min, bt_max)))
+            count += 1
+
+print("\n=======================================================")
+print("This is the Task Generation Output")
+print("Generate {} tasks.".format(count))
