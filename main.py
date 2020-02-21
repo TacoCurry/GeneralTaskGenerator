6d9f7ed4169bd49bcffacb057e978972e9a7d987
Jo-Yewon <yeawonjo@gmail.com>
2020-02-22 00:25:33 +0900

    feat: generate random non-rt tasks

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from input import *
+import random
 
 sim_time, arr_rate, bt_min, bt_max = get_input()
-~~~
+
+with open('generated.txt', 'w', encoding='utf-8') as f:
+    for cur_time in range(sim_time):
+        if random.random() <= arr_rate:
+            f.write("{} {}\n".format(cur_time, random.randint(bt_min, bt_max)))
