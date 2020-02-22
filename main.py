3dfb6bcfb890b5dd43fdf4a0c0d7d4398fed2126
Jo-Yewon <yeawonjo@gmail.com>
2020-02-22 22:29:55 +0900

    fix: 출력 결과 첫 줄에 생성한 태스크 수를 출력하도록 변경

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import random
 
 sim_time, arr_rate, bt_min, bt_max = get_input()
 
-with open('generated.txt', 'w', encoding='utf-8') as f:
-    count = 0
-    for cur_time in range(sim_time):
-        if random.random() <= arr_rate:
-            f.write("{} {}\n".format(cur_time, random.randint(bt_min, bt_max)))
-            count += 1
+with open('input_nonrtt.txt', 'w', encoding='utf-8') as f:
+    tasks = [(cur_time, random.randint(bt_min, bt_max))
+             for cur_time in range(sim_time) if random.random() <= arr_rate]
+
+    f.write("{}\n".format(len(tasks)))
+    for task in tasks:
+        f.write("{} {}\n".format(*task))
 
 print("\n=======================================================")
 print("This is the Task Generation Output")
-print("Generate {} tasks.".format(count))
+print("Generate {} tasks.".format(len(tasks)))
